6be4dc713d00986d56d84ceee52c3f02a5eff0a0
eduard <eduard@192-168-0-142.rdsnet.ro>
2023-05-27 19:56:09 +0300

    implemented image upload for create product

--- model/product.js
+++ model/product.js
@@ -43,9 +43,9 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    // images: {
-    //   type: Array,
-    // },
+    images: {
+      type: Array,
+    },
     shipping: {
       type: String,
       enum: ['Yes', 'No'], // enum means it has to be either one of these two values
